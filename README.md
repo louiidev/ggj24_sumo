7c7bf3a1b5edc11bd626f8f4533717867a2373f2
20 GGJ <ggj20@c07g60rdq6ny.sae.au>
2024-01-26 22:54:03 +1100

    added playerscore

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ggj24_sumo
+
+Player colors: 
+	* Green #00a700
+	* Red #ff604f
+	* Yellow #c0ac00
+	* Purple #f67aff
---
+++ Scenes/king_of_the_hill.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+signal updateScore(playerNum, ScoreAdd)
+
+func playerEntered():
+	pass
+	
+func playerExited():
+	pass
