8eae6978321f352dfe6adfbba335ee1c4799f5f7
Ronald Chau <ron.chau@rea-group.com>
2024-01-27 11:59:14 +1100

    Add player to main scene

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var playerNum = 0
+@export var moveSpeed:float = 400
+@export var tackleSpeed:float = 16000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +10,17 @@ func _ready():
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var look_direction = Input.get_vector("look_down","look_up","look_left", "look_right")
-	velocity = input_direction * speed
+	var look_direction = Input.get_vector(
+		"look_left", 
+		"look_right",
+		"look_up",
+		"look_down")
+	
+	if Input.is_action_just_pressed("tackle"):
+		velocity = look_direction * tackleSpeed
+	else:
+		velocity = input_direction * moveSpeed
+	
 	look_at(position + look_direction.normalized())
 
 func _physics_process(delta):
