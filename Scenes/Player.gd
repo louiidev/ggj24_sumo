fcbd821ac5bbf08ccf70d78b948c71076f4fa95f
Ronald Chau <ron.chau@rea-group.com>
2024-01-27 16:47:22 +1100

    Use physics for player movements

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,28 +1,34 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 @export var playerNum = 0
-@export var moveSpeed:float = 400
-@export var tackleSpeed:float = 16000
+@export var moveSpeed:float = 900
+@export var tackleSpeed:float = 1800
+
+var playerMoveDirection = Vector2.ZERO
+var playerLookDirection = Vector2.ZERO
+var lookDirection = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	gravity_scale = 0.0
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var look_direction = Input.get_vector(
+func _process(delta):
+	playerMoveDirection = Input.get_vector("left", "right", "up", "down").normalized()
+	playerLookDirection = Input.get_vector(
 		"look_left", 
-		"look_right",
-		"look_up",
-		"look_down")
-	
-	if Input.is_action_just_pressed("tackle"):
-		velocity = look_direction * tackleSpeed
-	else:
-		velocity = input_direction * moveSpeed
-	
-	look_at(position + look_direction.normalized())
+		"look_right", 
+		"look_up", 
+		"look_down"
+	).normalized()
 
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
+func _integrate_forces(state):
+	state.apply_force(playerMoveDirection * moveSpeed)
+
+	#var rotation_direction = Input.get_axis("look_left", "look_right")
+	
+	
+	#state.angular_velocity += rotation_direction * 300
+	#
+	if Input.is_action_just_pressed("tackle"):
+		state.apply_impulse(playerLookDirection * tackleSpeed)
+	
