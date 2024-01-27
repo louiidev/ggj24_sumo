3b29d31aabc2b83380bf90012f5dc5068eec94c2
Louis Johnson <louis@repeat.gg>
2024-01-27 17:00:29 +1000

    added portraits and logo to lobby

--- Scripts/PlayerSelectItem.gd
+++ Scripts/PlayerSelectItem.gd
@@ -1,16 +1,41 @@
 extends Panel
 
 
-#var device_id: int = 0;
+var portrait_paths = [
+	"green.jpg",
+	"red.webp",
+	"yellow.jpg",
+	"purple.webp"
+]
 
-@onready var label: Label = $Label
+var device_id: int = 0;
 
-func player_connected(color: String, device_id: int):
+@onready var label: Label = $Vbox/Label
+@onready var texture_rect: TextureRect = $Vbox/Texture
+
+func set_portrait():
+	print("res://images/portraits/" + portrait_paths[device_id])
+	var image = Image.load_from_file("res://images/portraits/" + portrait_paths[device_id])
+	var texture = ImageTexture.create_from_image(image)
+	texture_rect.texture = texture
+
+
+func _ready():
+	set_device_id(get_index())
+	print(device_id)
+	set_portrait()
+
+func set_device_id(id: int):
+	device_id = id
+
+func player_connected(color: String, id: int):
+	set_device_id(id)
 	var stylebox = StyleBoxFlat.new();
 	stylebox.bg_color = Color.from_string(color, Color.BLACK)
 	stylebox.corner_radius_bottom_left = 10
 	self.add_theme_stylebox_override("panel", stylebox)
 	label.text = "Player " + str(device_id + 1)
+	
 
 	
 	
