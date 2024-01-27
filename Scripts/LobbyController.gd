96ae97542f0e169d45acf6191b0659c48787590d
Louis Johnson <louis@repeat.gg>
2024-01-27 18:30:30 +1000

    added scene changing

--- Scripts/LobbyController.gd
+++ Scripts/LobbyController.gd
@@ -7,9 +7,15 @@ var player_select_item = preload("res://Scenes/PlayerSelectItem.tscn")
 
 @onready var UI: Control = $UI
 @onready var PlayerSelectContainer: Control = $UI/PlayerSelectContainer
+@onready var start_game_panel: Panel = $UI/StartGamePanel
 @onready var game_globals = get_node("/root/GameGlobals")
 
+@onready var main_scene = preload("res://Scenes/main.tscn")
+
 signal on_player_connected(device_id: int)
+
+
+var enabled = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,21 +29,42 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	handle_controller_input()
+	if !enabled:
+		return
 	
+	handle_controller_input()
 
 func join_player(device: int):
 	on_player_connected.emit(device)
 	player_panels[device].player_connected(game_globals.playerColors[device], device)
 
 func handle_controller_input():
+	check_start_game()
 	for i in player_panels.size():
 		if Input.is_joy_known(i) and Input.is_joy_button_pressed(i, JOY_BUTTON_START):
 			join_player(i)
 
 func _joy_connection_changed(device: int, connected: bool):
 	print(str(device)+" "+str(connected))
+	
+func start_game():
+	get_tree().root.add_child(main_scene.instantiate())
+	
+	get_node("/root/Lobby").hide()
+	enabled = false
+	pass
 
+
+func check_start_game():
+
+	if start_game_panel.visible:
+		if Input.is_action_just_pressed("ui_accept"):
+			start_game()
+		if Input.is_action_just_pressed("ui_cancel"):
+			start_game_panel.hide()
+	elif Input.is_action_just_pressed("ui_accept"):
+		start_game_panel.show()
+	
 
 #func despawn_player(device: int):
 	#players_joined[device] = false;
--- game_globals.gd
+++ game_globals.gd
@@ -1,6 +1,10 @@
 extends Node
 
 signal updateScore(playerNum)
+signal startGame()
 
 var playerScores = [0,0,0,0]
 var playerColors: Array[String] = ['#00a700', '#ff604f', '#c0ac00', '#f67aff']
+
+
+
